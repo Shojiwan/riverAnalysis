3d098b78611214d57ba77e5db95a7e7e7c18b905
Shojiwan <ryan.shojinaga@gmail.com>
2024-07-25 11:48:22 +1000

    update with cross section sample only

--- r/river_analysis.R
+++ r/river_analysis.R
@@ -7,18 +7,14 @@
 
 library(reshape2); library(raster); library(ggplot2)
 remove(list = ls()); cat("\014")
-
-path <- "C:/RMS/005_model/008_MD/03_models/002_tuflow/r/riverAnalysis/"
-source(paste0(path, "river_functions.R"))
-curt <- paste0(path, 'points.csv')        # Data frame of sample points
-dem  <- paste0(path, 'dem.tif')           # Elevation raster
+path <- "C:/RMS/006_scripts/R/riverAnalysis/"
+source(paste0(path, "r/river_functions.R"))
+curt <- paste0(path, 'data/points.csv')   # Data frame of sample points
+dem  <- paste0(path, 'data/gis/dem.tif')  # Elevation raster
 spce <- 2                                 # Spacing between sample points
 span <- 100                               # Distance of the span on each side
 
-xsct <- getXsct(curt, dem, spce, span)
-write.csv(x = xsct, file = paste0(path, 'xsct.csv'), row.names = F, quote = F)
-
-temp <- xsct[which(xsct$node == 22), ]
-windows(12, 12)
-ggplot(data = temp, aes(x = xdst, y = elev)) + geom_point() + geom_line()
+data <- getXsct(curt, dem, spce, span)
+# write.csv(x = xsct, file = paste0(path, 'data/xsct.csv'), 
+#           row.names = F, quote = F)
 
